4904c8d88e4d3e10a5be8103122de84f0f4fe99a
Diego Figueroa <a01660987@tec.mx>
2023-04-13 01:38:28 -0600

    crear sitio web para mostrar grafico

--- grafica/insertardatos.py
+++ grafica/insertardatos.py
@@ -3,16 +3,16 @@ from tabla.models import Alumnos
 alumno1 = Alumnos(id=1, nombre='Diego', apellido='Figueroa', horas=10)
 alumno1.save()
 
-alumno2 = Alumnos(id=2, nombre='Pablo', apellido='Sánchez', horas=25)
+alumno2 = Alumnos(id=2, nombre='Pablo', apellido='Sanchez', horas=25)
 alumno2.save()
 
-alumno3 = Alumnos(id=3, nombre='Carolina', apellido='González', horas=30)
+alumno3 = Alumnos(id=3, nombre='Carolina', apellido='Gonzalez', horas=30)
 alumno3.save()
 
 alumno4 = Alumnos(id=4, nombre='Sergio', apellido='Ruiz', horas=15)
 alumno4.save()
 
-alumno5 = Alumnos(id=5, nombre='Luis José', apellido='González', horas=38)
+alumno5 = Alumnos(id=5, nombre='Luis Jose', apellido='Gonzalez', horas=38)
 alumno5.save()
 
 alumno6 = Alumnos(id=6, nombre='Christian', apellido='Vallejo', horas=23)
--- grafica/tabla/views.py
+++ grafica/tabla/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from tabla.models import Alumnos
 
 # Create your views here.
+
+def datos(request):
+    datosdb = []
+    for alumno in Alumnos.objects.all():
+        datosdb.append({
+            "nombre": alumno.nombre,
+            "apellido": alumno.apellido,
+            "horas": alumno.horas
+        })
+    return JsonResponse({"datos":datosdb})
+
+def index(request):
+    return render(request, 'frontend/index.html')
